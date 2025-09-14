dda41b5b75fba04a904b946622a0b5ef669dd3fc
albertoclemente <clemalb@gmail.com>
2025-09-14 16:13:14 +0200

    chore(git): ignore tests/debug files and untrack from index

--- test_query.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -X POST http://localhost:8000/api/query \
-  -H "Content-Type: application/json" \
-  -d '{
-    "query": "How many documents do you have access to? List them if possible.",
-    "sessionId": "test-session-123"
-  }' \
-  -s | python -m json.tool
